8c93a02eae245cbcf313af5854ec76b4c8764c0e
Tim Kuminecz <tkuminecz@gmail.com>
2018-11-02 17:02:00 -0400

    support mobile devices

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,18 @@
-import Document from 'next/document'
+import * as React from 'react'
+import Document, { Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {}
+export default class MyDocument extends Document {
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </html>
+    )
+  }
+}
